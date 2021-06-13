d3ee832eac5e033b34092d28e97bd47e86e3fca8
useetv123 <aaldaff88@gmail.com>
2021-06-13 08:52:28 +0700

    commit de la guia

---
+++ gitUpload.sh
@@ -0,0 +1,5 @@
+cd C:\Users\\Administrator\Desktop\\EPG Usee TV\Source
+git add .
+git commit -m 'commit de la guia'
+git remote add origin https://github.com/Djawir888/useexml.git
+git push --force origin master
